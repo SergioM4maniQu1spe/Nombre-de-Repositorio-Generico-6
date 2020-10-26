ca581078b47861b232d3ee24e591a856f421d3b8
SergioM4maniQui1spe <sergiomamaniquispe680@gmail.com>
2020-10-26 16:11:25 -0300

    Cambios 3.0

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -6,7 +6,7 @@ const Footer = (props) => {
     <div className="text-center mb-3">
       <hr />
       <h4 className="text-muted">Hello React</h4>
-      <a href="https://github.com/santiagotrini/hello-react">
+      <a href="https://github.com/SergioM4maniQu1spe/Nombre-de-Repositorio-Generico-6">
         <i className="fa fa-github fa-3x text-dark"></i>
       </a>
     </div>
